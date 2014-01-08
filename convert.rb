ca9ac8b1a98384c9bddcdd830b67b03b84d1e5dc
Niouby <alonso.morgane@gmail.com>
2014-01-08 01:42:10 +0100

    suppression du model et modification du fichier ruby pour utiliser les fichier header et footer

--- convert.rb
+++ convert.rb
@@ -1,42 +1,19 @@
 #!/usr/bin/ruby
 
 require 'redcarpet'
-require 'pygments'
 
 class HTMLwithPygments < Redcarpet::Render::HTML
 	def doc_header()
-		"<html>
-			<head>
-				<meta charset=\"utf-8\" />
-				<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js\"></script>
-				<link rel=\"stylesheet\" href=\"http://yandex.st/highlightjs/7.5/styles/monokai_sublime.min.css\">
-				<script src=\"http://yandex.st/highlightjs/7.5/highlight.min.js\"></script>
-				<link href='http://fonts.googleapis.com/css?family=Gabriela' rel='stylesheet' type='text/css'>
-				<style>
-					body {
-						margin: 0 auto;
-						width: 768px;
-					}
-				</style>
-			</head>
-			<body>"
+		IO.read('doc_header.html')
 	end
-	#def block_code(code, language)
-	#	Pygments.highlight(code, :lexer => language)
-	#end
 	def doc_footer()
-		"<script type=\"text/javascript\">
-			$(document).ready(function() {
-				$('pre code').each(function(i, e) {hljs.highlightBlock(e)});
-				$('pre code').click(function() {  });
-			});
-		</script>
-		</body></html>"
+		IO.read('doc_footer.html')
 	end
 end
 
 markdown = Redcarpet::Markdown.new(
 	HTMLwithPygments,
+	:tables => true,
 	:with_toc_data => true,
 	:autolink => true,
 	:space_after_headers => true,
